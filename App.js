54c7717b2ca3ecea81fce9677cc7f940820bc1dd
Tim Burmester <Tim-Burmester1@web.de>
2022-08-09 04:03:21 +0200

    5.1: Set up basic react native app

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import the screens
+import Screen1 from './components/Start';
+import Screen2 from './components/Chat';
+
+// import react native gesture handler
+import 'react-native-gesture-handler';
+
+// import react Navigation
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
