36a1d04d663c7200e67a971ddfc7c20f29a5491a
Tim Burmester <Tim-Burmester1@web.de>
2022-08-10 22:02:39 +0200

    5.1: Added name display

--- components/Start.js
+++ components/Start.js
@@ -8,7 +8,8 @@ import 'react-native-gesture-handler';
 
 
 import React, { Component } from 'react';
-import {  View, Text,  Button,  } from 'react-native';
+import {  View, Text, TextInput, Button,  
+ } from 'react-native';
 
 import BackgroundImage from "../assets/Background Image.png";
 
@@ -16,12 +17,33 @@ import BackgroundImage from "../assets/Background Image.png";
 export default class Start extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { 
+      text: '' 
+      
+    
+    };
   }
  
   render() {
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        
+        
+       <TextInput
+         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+         onChangeText={(text) => this.setState({text})}
+         value={this.state.text}
+         placeholder='Type here ...'
+       />
+       <Text>Your name: {this.state.text}</Text>
+       <Button
+  onPress={() => {
+    this.alertMyText({text: this.state.text});
+  }}
+  title="Press Me"
+/>
+
+        
         <Text>Hello Start</Text>
         <Button
           title="Go to Chat"
