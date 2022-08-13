b8df5c6acca1b05b80a185a7ab73df9f97aa84cc
Tim Burmester <Tim-Burmester1@web.de>
2022-08-13 22:41:13 +0200

    5.2: Added gifted chats library and added chat interface
    Added gifted chats library and added chat interface

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { View, Text} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 
 export default class Chat extends React.Component {
 
-  componentDidMount(){
-    let name = this.props.route.params.name; // OR ...
-    // let { name } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name });
-}
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
 
 
 
@@ -19,6 +38,14 @@ export default class Chat extends React.Component {
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: bgColor}}>
         <Text>Hello Screen2!</Text>
+
+        <GiftedChat
+  messages={this.state.messages}
+  onSend={messages => this.onSend(messages)}
+  user={{
+    _id: 1,
+  }}
+/>
       </View>
     )
   }
