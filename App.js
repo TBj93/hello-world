0d50615c178aaccd9a5fece6f1e2ee26cde21e21
Tim Burmester <Tim-Burmester1@web.de>
2022-08-14 04:21:42 +0200

    5.2: minor fixes

--- App.js
+++ App.js
@@ -35,11 +35,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#faebd7",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
